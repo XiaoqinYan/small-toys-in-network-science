5a6dcacdf569ff1a06536c6917d9e45944d2a83f
Xiaoqin Yan <62516431+XiaoqinYan@users.noreply.github.com>
2020-06-02 09:46:48 -0400

    Create communities.py

---
+++ communities.py
@@ -0,0 +1,188 @@
+# Configure plotting in Jupyter
+from matplotlib import pyplot as plt
+#matplotlib inline
+plt.rcParams.update({
+    'figure.figsize': (7.5, 7.5),
+    'axes.spines.right': False,
+    'axes.spines.left': False,
+    'axes.spines.top': False,
+    'axes.spines.bottom': False})
+# Seed random number generator
+import random
+from numpy import random as nprand
+seed = hash("Network Science in Python") % 2**32
+nprand.seed(seed)
+random.seed(seed)
+
+import networkx as nx
+
+import networkx.algorithms.community as nxcom
+# Generate the network
+G_karate = nx.karate_club_graph()
+# Find the communities
+communities = sorted(nxcom.greedy_modularity_communities(G_karate), key=len, reverse=True)
+# Count the communities
+len(communities)
+
+def set_node_community(G, communities):
+    '''Add community to node attributes'''
+    for c, v_c in enumerate(communities):
+        for v in v_c:
+            # Add 1 to save 0 for external edges
+            G.nodes[v]['community'] = c + 1
+            
+def set_edge_community(G):
+    '''Find internal edges and add their community to their attributes'''
+    for v, w, in G.edges:
+        if G.nodes[v]['community'] == G.nodes[w]['community']:
+            # Internal edge, mark with community
+            G.edges[v, w]['community'] = G.nodes[v]['community']
+        else:
+            # External edge, mark as 0
+            G.edges[v, w]['community'] = 0
+            
+def get_color(i, r_off=1, g_off=1, b_off=1):
+    r0, g0, b0 = 0, 0, 0
+    n = 16
+    low, high = 0.1, 0.9
+    span = high - low
+    r = low + span * (((i + r_off) * 3) % n) / (n - 1)
+    g = low + span * (((i + g_off) * 5) % n) / (n - 1)
+    b = low + span * (((i + b_off) * 7) % n) / (n - 1)
+    return (r, g, b)
+
+# Set node and edge communities
+set_node_community(G_karate, communities)
+set_edge_community(G_karate)
+
+# Set community color for nodes
+node_color = [get_color(G_karate.nodes[v]['community']) for v in G_karate.nodes]
+
+# Set community color for internal edges
+external = [(v, w) for v, w in G_karate.edges if G_karate.edges[v, w]['community'] == 0]
+internal = [(v, w) for v, w in G_karate.edges if G_karate.edges[v, w]['community'] > 0]
+internal_color = [get_color(G_karate.edges[e]['community']) for e in internal]
+
+karate_pos = nx.spring_layout(G_karate)
+# Draw external edges
+nx.draw_networkx(
+    G_karate,
+    pos=karate_pos,
+    node_size=0,
+    edgelist=external,
+    edge_color="#333333")
+# Draw nodes and internal edges
+nx.draw_networkx(
+    G_karate,
+    pos=karate_pos,
+    node_color=node_color,
+    edgelist=internal,
+    edge_color=internal_color)
+
+# Load data file into network
+from pathlib import Path
+data_dir = Path('.') / 'data'
+G_social = nx.read_edgelist(data_dir / 'mcauley2012' / 'facebook_combined.txt')
+
+# Caluclate layout and draw
+pos = nx.spring_layout(G_social, k=0.1)
+nx.draw_networkx(
+    G_social, pos=pos, node_size=0, edge_color="#333333", alpha=0.05, with_labels=False)
+
+communities = sorted(nxcom.greedy_modularity_communities(G_social), key=len, reverse=True)
+len(communities)
+
+# Set node and edge communities
+set_node_community(G_social, communities)
+set_edge_community(G_social)
+
+# Set community color for internal edges
+external = [(v, w) for v, w in G_social.edges if G_social.edges[v, w]['community'] == 0]
+internal = [(v, w) for v, w in G_social.edges if G_social.edges[v, w]['community'] > 0]
+internal_color = [get_color(G_social.edges[e]['community']) for e in internal]
+
+# Draw external edges
+nx.draw_networkx(
+    G_social, pos=pos, node_size=0, edgelist=external, edge_color="#333333",
+    alpha=0.2, with_labels=False)
+# Draw internal edges
+nx.draw_networkx(
+    G_social, pos=pos, node_size=0, edgelist=internal, edge_color=internal_color,
+    alpha=0.05, with_labels=False)
+
+result = nxcom.girvan_newman(G_karate)
+communities = next(result)
+len(communities)
+
+# Set node and edge communities
+set_node_community(G_karate, communities)
+set_edge_community(G_karate)
+
+# Set community color for nodes
+node_color = [get_color(G_karate.nodes[v]['community']) for v in G_karate.nodes]
+
+# Set community color for internal edges
+external = [(v, w) for v, w in G_karate.edges if G_karate.edges[v, w]['community'] == 0]
+internal = [(v, w) for v, w in G_karate.edges if G_karate.edges[v, w]['community'] > 0]
+internal_color = [get_color(G_karate.edges[e]['community']) for e in internal]
+
+# Draw external edges
+nx.draw_networkx(
+    G_karate, pos=karate_pos, node_size=0,
+    edgelist=external, edge_color="#333333", with_labels=False)
+# Draw nodes and internal edges
+nx.draw_networkx(
+    G_karate, pos=karate_pos, node_color=node_color,
+    edgelist=internal, edge_color=internal_color)
+
+import itertools
+result = nxcom.girvan_newman(G_karate)
+communities = next(itertools.islice(result, 2, 3))
+
+# Set node and edge communities
+set_node_community(G_karate, communities)
+set_edge_community(G_karate)
+
+# Set community color for nodes
+node_color = [get_color(G_karate.nodes[v]['community']) for v in G_karate.nodes]
+
+# Set community color for internal edges
+external = [(v, w) for v, w in G_karate.edges if G_karate.edges[v, w]['community'] == 0]
+internal = [(v, w) for v, w in G_karate.edges if G_karate.edges[v, w]['community'] > 0]
+internal_color = [get_color(G_karate.edges[e]['community']) for e in internal]
+
+# Draw external edges
+nx.draw_networkx(
+    G_karate, pos=karate_pos, node_size=0,
+    edgelist=external, edge_color="#333333", with_labels=False)
+# Draw nodes and internal edges
+nx.draw_networkx(
+    G_karate, pos=karate_pos, node_color=node_color,
+    edgelist=internal, edge_color=internal_color)
+
+cliques = list(nx.find_cliques(G_karate))
+cliques
+
+# Find maximum clique
+max_clique = max(cliques, key=len)
+max_clique
+
+# Visualize maximum clique
+node_color = [(0.5, 0.5, 0.5) for v in G_karate.nodes()]
+for i, v in enumerate(G_karate.nodes()):
+    if v in max_clique:
+        node_color[i] = (0.5, 0.5, 0.9)
+nx.draw_networkx(G_karate, node_color=node_color, pos=karate_pos)
+
+# Find k-cores
+G_core_30 = nx.k_core(G_social, 30)
+G_core_60 = nx.k_core(G_social, 60)
+
+# Visualize network and k-cores
+nx.draw_networkx(
+    G_social, pos=pos, node_size=0, edge_color="#333333", alpha=0.05, with_labels=False)
+nx.draw_networkx(
+    G_core_30, pos=pos, node_size=0, edge_color="#7F7FEF", alpha=0.05, with_labels=False)
+nx.draw_networkx(
+    G_core_60, pos=pos, node_size=0, edge_color="#AFAF33", alpha=0.05, with_labels=False)
+
